63c181d80262ddad8990247e6dc4023112c79587
TrungB2007215 <luminhtrung0902@gmail.com>
2023-02-16 21:15:11 +0700

    cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const expess = require("express");
 const cors = require("cors");
 
+const contractsRouter = require("./app/routes/contract.route");
+const ApiError = require("./app/api-error");
 const app = expess();
 
 app.use(cors());
@@ -9,5 +11,24 @@ app.use(expess.json());
 app.get("/", (req, res) => {
     res.json({message: "Welcome to contact book application."});
 });
+
+app.use("/api/contacts", contractsRouter);
+
+//handle 404 response
+app.use((req, res, next) => {
+    //code ở đây sẽ chạy khi không có route được định nghĩa nào
+    //khớp với yêu cầu. gọi next() để chuyển sang middleware xử lý lỗi
+    return next(new ApiError(404, "Resource not found"));
+});
+
+//define error-handling middleware last, ofer other app.use() and routes calls
+app.use((err, req, res, next) =>{
+    //middleware xử lý lỗi tập trung
+    //trong các đoạn code xử lý ở các route, gọi next(error)
+    //sẽ chuyển về middleware xử lý lỗi này
+    return res.status(err.statusCode || 500).json({
+        message: error.message || "Internal Server Error"
+    })
+})
 
 module.exports = app;
---
+++ app/api-error.js
@@ -0,0 +1,9 @@
+class ApiError extends Error {
+    constructor(statusCode, massage) {
+        super();
+        this.statusCode = statusCode;
+        this.massage = massage;
+    }
+}
+
+module.exports = ApiError;
